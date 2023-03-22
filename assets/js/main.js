6434390598e69f21facc838a3cc71306271d25f5
xGIANOx <studioenricogian@gmail.com>
2023-03-22 18:12:25 +0100

    [fix] layout, img path

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,33 @@
 const team = [
     {
         name: "Wayne Barnett",
-        img: "../assets/img/wayne-barnett-founder-ceo.jpg",
+        img: "./assets/img/wayne-barnett-founder-ceo.jpg",
         role: "Founder & CEO"
     },
     {
         name: "Angela Caroll",
-        img: "../assets/img/angela-caroll-chief-editor.jpg",
+        img: "./assets/img/angela-caroll-chief-editor.jpg",
         role: "Chief Editor"
     },
     {
         name: "Walter Gordon ",
-        img: "../assets/img/walter-gordon-office-manager.jpg",
+        img: "./assets/img/walter-gordon-office-manager.jpg",
         role: "Office Manager"
     },
     {
         name: "Angela Lopez",
-        img: "../assets/img/angela-lopez-social-media-manager.jpg",
+        img: "./assets/img/angela-lopez-social-media-manager.jpg",
         role: "Social Media Manager"
     },
     {
         name: "Scott Estrada",
-        img: "../assets/img/scott-estrada-developer.jpg",
+        img: "./assets/img/scott-estrada-developer.jpg",
         role: "Developer"
     },
     {
         name: "Barbara Ramos",
-        img: "../assets/img/barbara-ramos-graphic-designer.jpg",
-        role: "DeGraphic Designer"
+        img: "./assets/img/barbara-ramos-graphic-designer.jpg",
+        role: "Graphic Designer"
     }
 ]
 const rowEl = document.querySelector('.row')
@@ -35,11 +35,12 @@ console.log(team);
 for (let i = 0; i < team.length; i++) {
     const thisMember = team[i];
     console.log(thisMember);
+    const orderClass = i % 2 === 0 ? 'odd' : 'even'; // add order class to alternate cards
     const markup = `
-    <div class="col pb-5">
+    <div class="col pb-5 ${orderClass}">
         <div class="card border-0 text-center bg-black text-white">
             <img src="${thisMember.img}" alt="">
-            <div class="card-body">
+            <div class="card-body ${orderClass}">
                 <h3>
                     ${thisMember.name} 
                 </h3>
@@ -49,6 +50,15 @@ for (let i = 0; i < team.length; i++) {
             </div>
             
         </div>
+    </div>
+    <div class="col pb-5 text-white align-items-center d-flex fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, at neque illo dolore ullam illum dolores a id, consequuntur ea aliquam eligendi iure quas repellendus inventore possimus nam error doloribus.
     </div>`
     rowEl.innerHTML += markup
 }
+
+const arrow = document.querySelector("a");
+
+arrow.addEventListener("click", () => { 
+  const nextSection = document.querySelector("section.wrapper").nextElementSibling;
+  nextSection.scrollIntoView({ behavior: "smooth" });
+});
